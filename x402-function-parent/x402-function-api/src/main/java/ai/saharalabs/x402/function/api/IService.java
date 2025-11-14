1150eb2647278123f4ee0aadd0cbf88803958be4
ronghuanz <ronghuanz@saharalabs.ai>
2025-11-14 13:15:17 +0800

    refactor: use parent module to manage x function dependencies.

--- x402-function-parent/x402-function-api/src/main/java/ai/saharalabs/x402/function/api/IService.java
+++ x402-function-parent/x402-function-api/src/main/java/ai/saharalabs/x402/function/api/IService.java
@@ -30,11 +30,10 @@ package ai.saharalabs.x402.function.api;
 import ai.saharalabs.x402.function.api.command.ServiceCreationCmd;
 import ai.saharalabs.x402.function.api.dto.ServiceCreateDTO;
 import ai.saharalabs.x402.function.api.dto.ServiceDTO;
-import ai.saharalabs.x402.function.vendor.VendorException;
 
 public interface IService {
 
-  ServiceCreateDTO create(ServiceCreationCmd cmd) throws VendorException;
+  ServiceCreateDTO create(ServiceCreationCmd cmd);
 
-  ServiceDTO status(String id) throws VendorException;
+  ServiceDTO status(String id);
 }
